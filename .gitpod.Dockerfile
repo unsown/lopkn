38302da2aacecf898d8b6394da01fe4acaa24167
laseryuan <laser.yuan@gmail.com>
2020-09-15 14:51:23 +0000

    Add config for gitpod

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,14 +1,18 @@
-FROM gitpod/workspace-full
+FROM gitpod/workspace-full-vnc:latest
 
 USER root
 
 # Install Dropbear SSH server
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -yq \
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get update -qy && apt-get install -qy \
         netcat \
         openssh-server \
         dropbear \
         tmux \
         vim
+
+RUN apt-get install -qy \
+      pulseaudio `# get a dummy sink`
 
 # Install Chisel
 RUN curl https://i.jpillora.com/chisel! | bash
--- temp.sh
+++ temp.sh
@@ -1,3 +1,20 @@
 Intall tkinter
 sudo apt-get install python-tk
 pip install tkinter
+
+pip install pygame
+pip install ipython
+pip freeze > requirements.txt    
+python tracking.py
+python tracking2.py
+
+# start pulseaudio for pygame
+pulseaudio -D
+python Space_Invaders/spaceinvaders.py
+
+docker run \
+  -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -e PULSE_SERVER=unix:/run/user/1000/pulse/native -v /run/user/1000/pulse:/run/user/1000/pulse \
+  -v $(pwd):/home/python/code \
+  --rm -it lasery/codebench:python bash
+
